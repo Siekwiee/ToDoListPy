9e3f00293da85bb3fd95a934658b8b07b41db527
JannikWiest <jannik.wiest@ipdynamics.de>
2023-08-09 09:32:07 +0200

    So far so good

--- ToDo.py
+++ ToDo.py
@@ -1,19 +1,54 @@
 import tkinter as tk
-
-def button_click():
-    label.config(text="Created new ToDo")
+from tkinter import messagebox
 
 canvas = tk.Tk()
-canvas.title("ToDo Liste")
-canvas.minsize(800,800)
-canvas.maxsize(1400, 1000)
+canvas.title("Todo List")
+canvas.geometry("800x1000")
 
-label = tk.Label(canvas, text="Hello")
+window_height = 0
+window_width = 0
+
+def add_task():
+    task = entry.get()
+    if task:
+        tasks_listbox.insert(tk.END, task)
+        entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Warning", "Please enter a task.")
+
+def delete_task():
+    selected_task_index = tasks_listbox.curselection()
+    if selected_task_index:
+        tasks_listbox.delete(selected_task_index)
+
+def get_window_dimensions():
+    global window_height, window_width
+    window_height = canvas.winfo_height()
+    window_width = canvas.winfo_width()
+
+    update_listbox_size()
+
+def update_listbox_size():
+    tasks_listbox_width = window_width - 100
+    tasks_listbox_height = window_height - 100
+    print(tasks_listbox_height, tasks_listbox_width)
+
+    tasks_listbox.config(width=tasks_listbox_width, height=tasks_listbox_height)
+
+label = tk.Label(canvas, text="Hello, Tkinter!")
 label.pack()
 
-button = tk.Button(canvas, text="New Todo", command=button_click)
-button.pack()
+entry = tk.Entry(canvas)
+entry.pack()
 
+add_button = tk.Button(canvas, text="Add Task", command=add_task)
+add_button.pack()
+
+delete_button = tk.Button(canvas, text="Delete Task", command=delete_task)
+delete_button.pack()
+
+tasks_listbox = tk.Listbox(canvas)
+tasks_listbox.pack()
+canvas.after(200, get_window_dimensions)
 
 canvas.mainloop()
-
